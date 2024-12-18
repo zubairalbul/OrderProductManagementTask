e52522b6042c8ad9cc4731c5418c355b9dd81f7e
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-12-18 14:31:29 +0400

    initial commit (foundation).

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace OrderProductManagementTask
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            //  Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
